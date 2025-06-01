69f708be0e634d1ac480cf892deeb169fc98e750
iko <mail@iko.soy>
2025-06-01 14:37:25 +0300

    got queries working

--- src/ProjectM36/Rest.hs
+++ src/ProjectM36/Rest.hs
@@ -7,20 +7,22 @@ import Data.Aeson qualified as A
 import Data.Aeson.Key qualified as Key
 import Data.Aeson.KeyMap qualified as KeyMap
 import Data.Aeson.Types qualified as A
-import Data.ByteString.Base64 (decodeBase64)
+import Data.Bifunctor (Bifunctor (..))
+import Data.ByteString.Base64 (decodeBase64, encodeBase64)
 import Data.Function
 import Data.Functor
+import Data.Set qualified as Set
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as T
 import Data.Traversable
 import Data.Vector qualified as V
-import Debug.Trace
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp qualified as Warp
 import Options.Applicative
 import ProjectM36.Base
 import ProjectM36.Client
+import ProjectM36.Relation (relationTrue)
 import Servant
 import Servant.Server.Generic
 
@@ -38,6 +40,7 @@ options =
 data Query
   = DefineQuery RelVarName [AttributeExpr]
   | InsertQuery RelVarName RelationalExpr
+  | QueryQuery RelationalExpr
 
 instance FromJSON Query where
   parseJSON = withObject "Query" $ \o -> case KeyMap.toList o of
@@ -49,7 +52,50 @@ instance FromJSON Query where
       name <- i .: "name"
       rel <- i .: "rel" >>= decodeExpr
       pure $ InsertQuery name rel
+    [("query", qry)] -> QueryQuery <$> decodeExpr qry
     _ -> fail "Poorly formed query key"
+
+encodeRelationType :: V.Vector Attribute -> A.Value
+encodeRelationType =
+  A.Array
+    . fmap
+      ( \(Attribute key t) ->
+          toJSON . (key,) $ case t of
+            IntAtomType -> A.String "int"
+            IntegerAtomType -> A.String "integer"
+            ScientificAtomType -> A.String "scientific"
+            DoubleAtomType -> A.String "double"
+            TextAtomType -> A.String "text"
+            DayAtomType -> A.String "day"
+            DateTimeAtomType -> A.String "datetime"
+            ByteStringAtomType -> A.String "bytestring"
+            BoolAtomType -> A.String "bool"
+            UUIDAtomType -> A.String "uuid"
+            RelationAtomType (Attributes attrs) -> encodeRelationType attrs
+            _ -> error $ "unsupported atom type: " <> show t
+      )
+
+encodeAtom :: Atom -> A.Value
+encodeAtom = \case
+  IntegerAtom a -> toJSON a
+  IntAtom a -> toJSON a
+  ScientificAtom a -> toJSON a
+  DoubleAtom a -> toJSON a
+  TextAtom a -> toJSON a
+  DayAtom a -> toJSON a
+  DateTimeAtom a -> toJSON a
+  ByteStringAtom a -> toJSON $ encodeBase64 a
+  BoolAtom a -> toJSON a
+  UUIDAtom a -> toJSON a
+  RelationAtom a -> encodeRelation a
+  a -> error $ "Unsupported atom type: " <> show a
+
+encodeRelation :: Relation -> A.Value
+encodeRelation (Relation (Attributes attrs) (RelationTupleSet tuples)) =
+  A.object
+    [ "attrs" A..= fmap (\(Attribute key _) -> key) attrs,
+      "vals" A..= (tuples <&> (\(RelationTuple _ atoms) -> encodeAtom <$> atoms))
+    ]
 
 decodeRelationType :: A.Array -> A.Parser (V.Vector Attribute)
 decodeRelationType o =
@@ -71,11 +117,11 @@ decodeRelationType o =
             _ -> fail $ "unknown type: " <> show v
       )
 
-decodeExpr :: A.Object -> A.Parser RelationalExpr
-decodeExpr o = case KeyMap.toList (traceShowId o) of
+decodeExpr :: A.Value -> A.Parser RelationalExpr
+decodeExpr = withObject "Expr" $ \o -> case KeyMap.toList o of
   [("lit", A.Object lit)] -> do
-    (traceShowId -> !attrs) <- lit .: "attrs" >>= decodeRelationType
-    (traceShowId -> !vals) :: [V.Vector Value] <- lit .: "vals"
+    attrs <- lit .: "attrs" >>= decodeRelationType
+    vals :: [V.Vector Value] <- lit .: "vals"
     tuples <- for vals $ \rel ->
       traverse
         (uncurry decodeAtom)
@@ -84,10 +130,31 @@ decodeExpr o = case KeyMap.toList (traceShowId o) of
       MakeStaticRelation
         (Attributes attrs)
         (RelationTupleSet $ RelationTuple (Attributes attrs) <$> tuples)
+  [("join", A.Array (V.toList -> [x, y]))] -> Join <$> decodeExpr x <*> decodeExpr y
+  [("union", A.Array (V.toList -> [x, y]))] -> Union <$> decodeExpr x <*> decodeExpr y
+  [("proj", A.Object p)] -> do
+    proj <- AttributeNames <$> p .: "proj"
+    expr <- p .: "expr" >>= decodeExpr
+    pure $ Project proj expr
+  [("rename", A.Object r)] -> do
+    mapping <- r .: "mapping" <&> Set.fromList . fmap (first Key.toText) . KeyMap.toList
+    expr <- r .: "expr" >>= decodeExpr
+    pure $ Rename mapping expr
+  [("diff", A.Array (V.toList -> [x, y]))] -> Difference <$> decodeExpr x <*> decodeExpr y
+  [("group", A.Object g)] -> do
+    attrs <- AttributeNames <$> g .: "attrs"
+    attr <- g .: "attr"
+    expr <- g .: "expr" >>= decodeExpr
+    pure $ Group attrs attr expr
+  [("ungroup", A.Object u)] -> do
+    attr <- u .: "attr"
+    expr <- u .: "expr" >>= decodeExpr
+    pure $ Ungroup attr expr
+  [("var", A.String v)] -> pure $ RelationVariable v ()
   _ -> fail $ "unknown expression: " <> show o
 
 decodeAtom :: AtomType -> A.Value -> A.Parser Atom
-decodeAtom (traceShowId -> t) (traceShowId -> v) = case t of
+decodeAtom t v = case t of
   IntAtomType -> IntAtom <$> parseJSON v
   IntegerAtomType -> IntegerAtom <$> parseJSON v
   ScientificAtomType -> ScientificAtom <$> parseJSON v
@@ -120,7 +187,7 @@ data API r = API
   { startTransaction :: r :- "transaction" :> Post '[JSON] SessionId,
     commit :: r :- "transaction" :> Capture "SessionId" SessionId :> PutNoContent,
     abort :: r :- "transaction" :> Capture "SessionId" SessionId :> DeleteNoContent,
-    query :: r :- "transaction" :> Capture "SessionId" SessionId :> ReqBody '[JSON] Query :> Post '[JSON] [A.Value]
+    query :: r :- "transaction" :> Capture "SessionId" SessionId :> ReqBody '[JSON] Query :> Post '[JSON] A.Value
   }
   deriving stock (Generic)
 
@@ -131,13 +198,15 @@ server conn =
         liftIO (createSessionAtHead conn defaultHeadName) >>= rethrow,
       commit = \sess -> (liftIO (autoMergeToHead sess conn UnionMergeStrategy defaultHeadName) >>= rethrow) $> NoContent,
       abort = \sess -> liftIO (closeSession sess conn) $> NoContent,
-      query = \sess -> \case
-        DefineQuery varName attrs -> do
-          liftIO (executeDatabaseContextExpr sess conn (Define varName attrs)) >>= rethrow
-          pure []
-        InsertQuery varName rel -> do
-          liftIO (executeDatabaseContextExpr sess conn (Insert varName rel)) >>= rethrow
-          pure []
+      query = \sess q ->
+        encodeRelation <$> case q of
+          DefineQuery varName attrs -> do
+            liftIO (executeDatabaseContextExpr sess conn (Define varName attrs)) >>= rethrow
+            pure relationTrue
+          InsertQuery varName rel -> do
+            liftIO (executeDatabaseContextExpr sess conn (Insert varName rel)) >>= rethrow
+            pure relationTrue
+          QueryQuery rel -> liftIO (executeRelationalExpr sess conn rel) >>= rethrow
     }
 
 rethrow :: (Show err) => Either err x -> Handler x
