b137dc5eb0ebfe7071e758b0011cddc18a2eb087
iko <mail@iko.soy>
2025-06-24 15:05:15 +0300

    Added nix build

--- flake.nix
+++ flake.nix
@@ -2,16 +2,53 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "flake-utils";
+    stacklock2nix.url = "github:cdepillabout/stacklock2nix";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs, stacklock2nix }:
     let
       supportedSystems =
         [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
       compilerVersion = "966";
     in flake-utils.lib.eachSystem supportedSystems (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ stacklock2nix.overlay ];
+        };
+        appendGhcFlags = opts: drv:
+          pkgs.haskell.lib.appendBuildFlag drv
+          ''--ghc-options="${builtins.concatStringsSep " " opts}"'';
+        insaneOptimizationFlags =
+          [ "-O2" "-fexpose-all-unfoldings" "-fspecialise-aggressively" ];
+        justStatic = package:
+          (pkgs.haskell.lib.overrideCabal
+            (pkgs.haskell.lib.justStaticExecutables package)
+            (drv: { disallowGhcReference = false; }));
       in {
+        packages = {
+          default = justStatic (pkgs.stacklock2nix {
+            stackYaml = ./stack.yaml;
+            baseHaskellPkgSet = pkgs.haskell.packages."ghc${compilerVersion}";
+            additionalHaskellPkgSetOverrides = (hfinal: hprev:
+              builtins.mapAttrs (name: value:
+                if (builtins.tryEval (!(builtins.isAttrs value)
+                  || !(value ? pname))).value or true then
+                  value
+                else if value ? doCheck && value.pname != "ghc" then
+                  pkgs.lib.pipe value (with pkgs.haskell.lib; [
+                    dontCheck
+                    dontHaddock
+                    doJailbreak
+                    doStrip
+                    disableLibraryProfiling
+                    disableExecutableProfiling
+                    (appendGhcFlags insaneOptimizationFlags)
+                  ])
+                else
+                  value) hprev);
+          }).pkgSet.project-m36-rest;
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             haskell.compiler."ghc${compilerVersion}"
@@ -21,4 +58,9 @@
           ];
         };
       });
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys =
+      [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
 }
